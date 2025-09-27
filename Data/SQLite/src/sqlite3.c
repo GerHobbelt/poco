4881fbacd11c139435203ef29949db75a2ada074
Ger Hobbelt <ger@hobbelt.com>
2025-09-27 22:04:10 +0200

    spelling correction

--- Data/SQLite/src/sqlite3.c
+++ Data/SQLite/src/sqlite3.c
@@ -218137,7 +218137,7 @@ static char *rbuObjIterGetIndexCols(
 
 /*
 ** Assuming the current table columns are "a", "b" and "c", and the zObj
-** paramter is passed "old", return a string of the form:
+** parameter is passed "old", return a string of the form:
 **
 **     "old.a, old.b, old.b"
 **
@@ -254723,7 +254723,7 @@ struct Fts5VocabCursor {
   void *pStruct;                  /* From sqlite3Fts5StructureRef() */
 
   int nLeTerm;                    /* Size of zLeTerm in bytes */
-  char *zLeTerm;                  /* (term <= $zLeTerm) paramater, or NULL */
+  char *zLeTerm;                  /* (term <= $zLeTerm) parameter, or NULL */
 
   /* These are used by 'col' tables only */
   int iCol;
